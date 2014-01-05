532dcc02c169e842cab1eb3f353767ea2d30674c
Thomas Holmes <thomas@devminded.com>
2014-01-04 22:41:22 -0500

    Add yard documentation

--- Gemfile
+++ Gemfile
@@ -3,6 +3,12 @@ source 'https://rubygems.org'
 # Specify your gem's dependencies in rspec-spy_on.gemspec
 gemspec
 
+group :documentation do
+  gem 'yard',           '0.8.7.3', :require => false
+  gem 'redcarpet',      '2.3.0'
+  gem 'github-markup',  '1.0.0'
+end
+
 case version = ENV['RSPEC_VERSION']
 when nil, false, ''
   gem 'rspec', '3.0.0.beta1'
--- lib/rspec/spy_on/example_methods.rb
+++ lib/rspec/spy_on/example_methods.rb
@@ -1,13 +1,38 @@
 module RSpec::SpyOn
   module ExampleMethods
+    # Enables an RSpec::Mocks::TestDouble or any other object to act as
+    # a test spy. Delegates to RSpec::Mocks to configure the allowing of
+    # the messages
+    #
+    # In particular, `spy_on` makes this act of configuring real objects
+    # as spies easier by providing more concise syntax for configuring
+    # spying on multiple methods while forwarding the messages along
+    # to the original implementations.
+    #
+    # @example
+    #     person = Person.new("Ally", 22)
+    #     spy_on(person, :name, :age)
+    #     # use `person` in a test
+    #     expect(person).to have_received(:name)
+    #     expect(person).to have_received(:age)
+    #
+    # @param target [Object/RSpec::Mocks::TestDouble] The object to be spied on
+    # @param messages [Array] Array of symbols for messages that should configured for spying
+    # @return (target)
     def spy_on(target, *messages)
       if target.is_a?(RSpec::Mocks::TestDouble)
         messages.each { |m| RSpec::Mocks.allow_message(target, m) }
       else
         messages.each { |m| allow_partial(target, m) }
       end
+      target
     end
 
+    private
+    # If a non RSpec::Mocks::TestDouble is to be a spy then it any messags
+    # sent should be forwarded to the original object
+    #
+    # @private
     def allow_partial(target, message)
       if target.respond_to?(message)
         RSpec::Mocks.allow_message(target, message).and_call_original
